b2b10e07881b219047b49db872f4820d3db5e31b
Eric Demauro <erickdemauro@gmail.com>
2017-01-22 11:02:17 -0800

    finished m behind algorithm.

--- LinkedList.java
+++ LinkedList.java
@@ -4,9 +4,15 @@ public class LinkedList {
 
   public static void main(String[] args) {
     LinkedList list = new LinkedList();
+    for(int i = 5; i > 0; i--) {
+      list.insert(new Node(i));
+    }
+
+    System.out.println(list.mToLast(4));
+
+    LinkedList list2 = new LinkedList();
+
     list.insert(new Node(5));
-    list.insert(new Node(6));
-    System.out.println(list.getHead().next());
   }
 
   public LinkedList() {
@@ -15,6 +21,43 @@ public class LinkedList {
 
   public Node getHead() {
     return head;
+  }
+
+  public Node getTail() {
+    return tail;
+  }
+
+  public void setTail(Node elem) {
+    tail = elem;
+  }
+
+  public Node mToLast(int n) {
+    Node curr, behindPtr;
+
+    curr = behindPtr = head;
+
+    for(int i = 0; i < n; i++) {
+      curr = curr.next();
+    }
+
+    if(curr == null) {
+      return null;
+    }
+
+    while(curr.next() != null) {
+      curr = curr.next();
+      behindPtr = behindPtr.next();
+    }
+
+    return behindPtr;
+  }
+
+  public void flatten() {
+
+  }
+
+  public void unflatten() {
+
   }
 
   public Node insert(Node elem) {
@@ -35,8 +78,14 @@ public class LinkedList {
     Node child;
 
     public Node(int data) {
+      this(data, null, null, null);
+    }
+
+    public Node(int data, Node next, Node prev, Node child) {
       this.data = data;
-      next = prev = child = null;
+      this.next = next;
+      this.prev = prev;
+      this.child = child;
     }
 
     public Node next() {
