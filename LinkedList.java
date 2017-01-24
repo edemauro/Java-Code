3054f04c0692de47b4f836dad29bd018c09ef07d
Eric Demauro <erickdemauro@gmail.com>
2017-01-23 21:36:33 -0800

    refactored linked list.

--- LinkedList.java
+++ LinkedList.java
@@ -28,49 +28,37 @@ public class LinkedList {
     Node f = new Node(6);
     Node g = new Node(25);
 
-    f.setNext(g);
-    g.setPrev(f);
-    a.setChild(f);
+    f.next = g;
+    g.prev = f;
+    a.child = f;
 
     list2.flatten();
 
-    Node ptr = list2.getHead();
+    Node ptr = list2.head;
 
     while(ptr != null) {
       System.out.println(ptr);
-      ptr = ptr.next();
+      ptr = ptr.next;
     }
 
     System.out.println("");
-    System.out.println(list2.getTail());
+    System.out.println(list2.tail);
     System.out.println("");
     list2.unflatten();
 
-    ptr = list2.getHead();
+    ptr = list2.head;
 
     while(ptr != null) {
       System.out.println(ptr);
-      ptr = ptr.next();
+      ptr = ptr.next;
     }
 
     System.out.println("");
-    System.out.println(list2.getTail());
+    System.out.println(list2.tail);
   }
 
   public LinkedList() {
     head = tail = null;
-  }
-
-  public Node getHead() {
-    return head;
-  }
-
-  public Node getTail() {
-    return tail;
-  }
-
-  public void setTail(Node elem) {
-    tail = elem;
   }
 
   public Node mToLast(int n) {
@@ -79,16 +67,16 @@ public class LinkedList {
     curr = behindPtr = head;
 
     for(int i = 0; i < n; i++) {
-      curr = curr.next();
+      curr = curr.next;
     }
 
     if(curr == null) {
       return null;
     }
 
-    while(curr.next() != null) {
-      curr = curr.next();
-      behindPtr = behindPtr.next();
+    while(curr.next != null) {
+      curr = curr.next;
+      behindPtr = behindPtr.next;
     }
 
     return behindPtr;
@@ -97,22 +85,22 @@ public class LinkedList {
   public void flatten() {
     Node curr = head;
 
-    while(curr.next() != null) {
+    while(curr.next != null) {
       if(curr.child != null) {
         appendToList(curr.child, tail);
       }
-      curr = curr.next();
+      curr = curr.next;
     }
 
     tail = curr;
   }
 
   public void appendToList(Node child, Node tail) {
-    tail.setNext(child);
-    child.setPrev(tail);
+    tail.next = child;
+    child.prev = tail;
 
     while(child != null) {
-      child = child.next();
+      child = child.next;
     }
 
     tail = child;
@@ -123,7 +111,7 @@ public class LinkedList {
 
     explore(head);
 
-    for(; curr != null; curr = curr.next())
+    for(; curr != null; curr = curr.next)
     tail = curr;
   }
 
@@ -132,11 +120,11 @@ public class LinkedList {
     while(curr != null) {
       if(curr.child != null) {
         curr.child.prev.next = null;
-        curr.child.setPrev(null);
+        curr.child.prev = null;
         explore(curr.child);
       }
 
-      curr = curr.next();
+      curr = curr.next;
     }
   }
 
@@ -159,38 +147,8 @@ public class LinkedList {
     private Node child;
 
     public Node(int data) {
-      this(data, null, null, null);
-    }
-
-    public Node(int data, Node next, Node prev, Node child) {
       this.data = data;
-      this.next = next;
-      this.prev = prev;
-      this.child = child;
-    }
-
-    public Node next() {
-      return next;
-    }
-
-    public Node prev() {
-      return prev;
-    }
-
-    public Node child() {
-      return child;
-    }
-
-    public void setNext(Node elem) {
-      next = elem;
-    }
-
-    public void setPrev(Node elem) {
-      prev = elem;
-    }
-
-    public void setChild(Node elem) {
-      child = elem;
+      next = prev = child = null;
     }
 
     public String toString() {
